4ad49a46762ef6209a5fa22fb9a23a1a705131f6
icenspln <icenboat@gmail.com>
2025-10-29 10:58:41 +0100

    helper function udpate

--- api/helpers.js
+++ api/helpers.js
@@ -1,10 +1,15 @@
 const errorHandler = (err, req, res, next) => {
   if (err) {
-    console.log(err);
+    // if this is reached, the err is actually just a wrong URI, im sending 404 instead of 500
+    if (err.name === "CastError" && err.path === "_id") {
+      return res.status(404).json({ success: false, message: "Not found!" });
+    }
+
     res.status(500).json({ success: false, message: "Internal error!" });
   }
-};
 
-// NOTE figure out a error handler middlware to stop the non responding requrests
+  // fallback behavior
+  res.status(500).json({ success: false, message: "Internal error!" });
+};
 
 module.exports = { errorHandler };
