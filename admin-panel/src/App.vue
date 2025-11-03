dd849e6ab24e0784ed7a4347a22badec51ef3ca7
icenspln <icenboat@gmail.com>
2025-11-03 10:25:42 +0100

    login+logout nav button

--- admin-panel/src/App.vue
+++ admin-panel/src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Toast from 'primevue/toast';
+// import { ref, watch } from 'vue';
+import LogoutButton from './components/LogoutButton.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+
+const currentRoute = useRoute();
+const logoutButtonVisible = computed(() => {
+  return currentRoute.path !== "/auth/login"
+
+})
+
+
+
+
 </script>
 
 <template>
+  <nav class="p-4">
+    <h1 class="text-amber-500">Administration Panel</h1>
+    <!-- <LogoutButton v-if="router.currentRoute !== '/auth/login' ? true : false" /> -->
+    <LogoutButton v-if="logoutButtonVisible" />
+    <!-- <p class="text-amber-500">only authorized admins are allowed to proceed</p> -->
+  </nav>
   <RouterView />
   <Toast />
 </template>
---
+++ admin-panel/src/components/LogoutButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import router from '@/router';
+import { useAuthStore } from '@/stores/auth';
+import { useToast } from 'primevue';
+import Button from "primevue/button"
+
+const toast = useToast();
+const logout = () => {
+  const authStore = useAuthStore()
+  authStore.logout()
+  router.push({ path: "/auth/login" })
+  toast.add({ severity: 'success', summary: "Logout successfull.", life: 3000 })
+}
+</script>
+
+<template>
+  <Button severity="danger" @click="logout">Logout</Button>
+
+</template>
